1dbe00f3a36f0afe94d9b6efec0ed5285f3a49bd
Thursdays-Child <36120433+Thursdays-Child@users.noreply.github.com>
2018-02-04 13:08:12 -0500

    Placeholder roadpost
    Feel free to delete

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 *  This file is copyright under the latest version of the EUPL.
 *  Please see LICENSE file for your rights under this license. */
 
+// This is the third 'step' of the FTL program, executes after the code in FTL.h:
 #include "FTL.h"
 
 char * username;
